033692293477644385f9b93699f736ef25f047e6
iushu <13622409484@163.com>
2018-09-29 15:41:18 +0800

    initialize java mail component

---
+++ mail/src/main/java/org/iushu/mail/server/ServerPlaceholder.java
@@ -0,0 +1,12 @@
+package org.iushu.mail.server;
+
+/**
+ * Created by iuShu on 18-9-29
+ */
+public class ServerPlaceholder {
+
+    public static void main(String[] args) {
+        System.out.println("placeholder class");
+    }
+
+}
---
+++ mail/src/main/java/org/iushu/mail/tutorial/JavaMailTutorial.java
@@ -0,0 +1,12 @@
+package org.iushu.mail.tutorial;
+
+/**
+ * Created by iuShu on 18-9-29
+ */
+public class JavaMailTutorial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java mail component.");
+    }
+
+}
