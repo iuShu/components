6bfcd2c486d93e04c0e9b9139950363136426598
iushu <13622409484@163.com>
2021-04-07 18:43:41 +0800

    daily update

--- springboot/src/main/java/org/iushu/project/Application.java
+++ springboot/src/main/java/org/iushu/project/Application.java
@@ -2,17 +2,21 @@ package org.iushu.project;
 
 import org.iushu.project.bean.Staff;
 import org.iushu.project.service.StaffService;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationImportSelector;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ConfigurationClassPostProcessor;
 import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.core.io.ProtocolResolver;
+import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.type.AnnotationMetadata;
 
 import java.util.Collection;
+import java.util.Set;
 
 /**
  * @author iuShu
@@ -33,6 +37,8 @@ public class Application {
     }
 
     /**
+     * @see SpringApplication#run(String...) core startup method
+     *
      * 1. loading ApplicationContextInitializer from built-in configuration.
      * @see SpringApplication#getSpringFactoriesInstances(Class)
      * @see org.springframework.core.io.support.SpringFactoriesLoader#loadFactoryNames(Class, ClassLoader)
@@ -44,10 +50,74 @@ public class Application {
      * @see SpringApplication#getSpringFactoriesInstances(Class)
      * @see org.springframework.core.io.support.SpringFactoriesLoader#loadFactoryNames(Class, ClassLoader)
      * @see SpringApplication#setListeners(Collection)
+     * @see SpringBootComponents#listeners() for more details
      *
-     * 3.
+     * 3. loading SpringApplicationRunListener from built-in configuration
+     * @see SpringApplication#getRunListeners(String[])
+     * @see org.springframework.boot.SpringApplicationRunListener
+     * @see SpringBootComponents#springApplicationRunListener() for more details
+     *
+     * 4. prepare Environment
+     * @see SpringApplication#prepareEnvironment
+     * @see SpringApplication#getOrCreateEnvironment()
+     *
+     * 5. prepare banner to be printed
+     * @see SpringApplication#printBanner(ConfigurableEnvironment)
+     * @see org.springframework.boot.Banner
+     * @see org.springframework.boot.SpringBootBanner default banner
+     *
+     * 6. create ApplicationContext
+     * @see SpringApplication#createApplicationContext()
+     * @see SpringApplication#DEFAULT_CONTEXT_CLASS
+     * @see org.springframework.context.annotation.AnnotationConfigApplicationContext default context in SpringBoot
+     *
+     * 7. loading SpringBootExceptionReporter from built-in configuration
+     * @see org.springframework.boot.SpringBootExceptionReporter
+     * @see org.springframework.boot.diagnostics.FailureAnalyzers composite
+     * @see org.springframework.boot.diagnostics.FailureAnalyzer default 19
+     * @see org.springframework.boot.diagnostics.FailureAnalysis
+     * @see org.springframework.boot.diagnostics.FailureAnalysisReporter
+     * @see SpringBootComponents#failureAnalyzers() for more details
+     *
+     * 8. prepare ApplicationContext
+     * @see SpringApplication#prepareContext
+     * @see SpringApplication#postProcessApplicationContext
+     * @see SpringApplication#applyInitializers
+     * @see SpringApplication#lazyInitialization
+     * @see org.springframework.boot.LazyInitializationBeanFactoryPostProcessor
+     *
+     * 9. loading Bean ino BeanFactory
+     * @see SpringApplication#load(ApplicationContext, Object[])
+     * @see org.springframework.boot.BeanDefinitionLoader#load()
+     * @see org.springframework.boot.BeanDefinitionLoader#annotatedReader
+     * @see org.springframework.context.annotation.AnnotatedBeanDefinitionReader
+     * @see org.springframework.boot.BeanDefinitionLoader#xmlReader
+     * @see org.springframework.beans.factory.xml.XmlBeanDefinitionReader
+     * @see org.springframework.boot.BeanDefinitionLoader#scanner
+     * @see org.springframework.context.annotation.ClassPathBeanDefinitionScanner
+     *
+     * 10. refresh Context
+     * @see SpringApplication#refreshContext(ConfigurableApplicationContext)
+     * @see SpringApplication#refresh(ApplicationContext)
+     * @see SpringApplication#refresh(ConfigurableApplicationContext)
+     * @see ConfigurableApplicationContext#refresh()
      */
     static void startup() {
+
+    }
+
+    /**
+     * @see ConfigurationClassPostProcessor
+     * @see AutoConfigurationImportSelector
+     * @see AutoConfigurationImportSelector.AutoConfigurationGroup
+     *
+     * Loading components from built-in configuration: spring-boot/META-INF/spring.factories
+     * @see AutoConfigurationImportSelector#getAutoConfigurationEntry(AnnotationMetadata)
+     * @see AutoConfigurationImportSelector#getCandidateConfigurations(AnnotationMetadata, AnnotationAttributes)
+     * @see AutoConfigurationImportSelector#getSpringFactoriesLoaderFactoryClass()
+     * @see org.springframework.core.io.support.SpringFactoriesLoader#loadFactoryNames(Class, ClassLoader)
+     */
+    static void importSelector() {
 
     }
 
--- springboot/src/main/java/org/iushu/project/SpringBootComponents.java
+++ springboot/src/main/java/org/iushu/project/SpringBootComponents.java
@@ -1,5 +1,9 @@
 package org.iushu.project;
 
+import org.springframework.boot.diagnostics.FailureAnalysis;
+
+import java.util.Collection;
+
 /**
  * @author iuShu
  * @since 4/6/21
@@ -44,6 +48,66 @@ public class SpringBootComponents {
      * @see org.springframework.boot.liquibase.LiquibaseServiceLocatorApplicationListener
      */
     static void listeners() {
+
+    }
+
+    /**
+     * Responsible for multicast application event
+     * @see org.springframework.boot.SpringApplicationRunListener
+     * @see org.springframework.boot.SpringApplicationRunListeners composite
+     *
+     * Default implementation
+     * @see org.springframework.boot.context.event.EventPublishingRunListener
+     *
+     * @see org.springframework.boot.context.event.ApplicationStartingEvent
+     * @see org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent
+     * @see org.springframework.boot.context.event.ApplicationContextInitializedEvent
+     * @see org.springframework.boot.context.event.ApplicationPreparedEvent
+     * @see org.springframework.boot.context.event.ApplicationStartedEvent
+     * @see org.springframework.boot.context.event.ApplicationReadyEvent
+     * @see org.springframework.boot.context.event.ApplicationFailedEvent
+     */
+    static void springApplicationRunListener() {
+
+    }
+
+    /**
+     * @see org.springframework.boot.diagnostics.FailureAnalyzers composite
+     * @see org.springframework.boot.diagnostics.FailureAnalyzer
+     *
+     * @see org.springframework.boot.diagnostics.FailureAnalysis failure analyzied info bean
+     * @see org.springframework.boot.diagnostics.FailureAnalysisReporter strategies to report an analysis
+     *
+     * Failure process in SpringBoot
+     * @see org.springframework.boot.SpringApplication#handleRunFailure
+     * @see org.springframework.boot.SpringApplication#reportFailure(Collection, Throwable)
+     * @see org.springframework.boot.SpringApplication#reportFailure
+     * @see org.springframework.boot.SpringBootExceptionReporter#reportException(Throwable)
+     * @see org.springframework.boot.diagnostics.FailureAnalyzers#reportException(Throwable)
+     * @see org.springframework.boot.diagnostics.FailureAnalysisReporter#report(FailureAnalysis)
+     * @see org.springframework.boot.diagnostics.LoggingFailureAnalysisReporter#report(FailureAnalysis) default implementation
+     *
+     * Default implementations(19) during startup, mostly have their binding exception.
+     * @see org.springframework.boot.context.properties.NotConstructorBoundInjectionFailureAnalyzer
+     * @see org.springframework.boot.diagnostics.analyzer.BeanCurrentlyInCreationFailureAnalyzer
+     * @see org.springframework.boot.diagnostics.analyzer.BeanDefinitionOverrideFailureAnalyzer
+     * @see org.springframework.boot.diagnostics.analyzer.BindFailureAnalyzer
+     * @see org.springframework.boot.diagnostics.analyzer.BindValidationFailureAnalyzer
+     * @see org.springframework.boot.diagnostics.analyzer.UnboundConfigurationPropertyFailureAnalyzer
+     * @see org.springframework.boot.diagnostics.analyzer.ConnectorStartFailureAnalyzer
+     * @see org.springframework.boot.diagnostics.analyzer.NoSuchMethodFailureAnalyzer
+     * @see org.springframework.boot.diagnostics.analyzer.NoUniqueBeanDefinitionFailureAnalyzer
+     * @see org.springframework.boot.diagnostics.analyzer.PortInUseFailureAnalyzer
+     * @see org.springframework.boot.diagnostics.analyzer.InvalidConfigurationPropertyNameFailureAnalyzer
+     * @see org.springframework.boot.diagnostics.analyzer.InvalidConfigurationPropertyValueFailureAnalyzer
+     * @see org.springframework.boot.autoconfigure.data.redis.RedisUrlSyntaxFailureAnalyzer
+     * @see org.springframework.boot.autoconfigure.diagnostics.analyzer.NoSuchBeanDefinitionFailureAnalyzer
+     * @see org.springframework.boot.autoconfigure.flyway.FlywayMigrationScriptMissingFailureAnalyzer
+     * @see org.springframework.boot.autoconfigure.jdbc.DataSourceBeanCreationFailureAnalyzer
+     * @see org.springframework.boot.autoconfigure.r2dbc.ConnectionFactoryBeanCreationFailureAnalyzer
+     * @see org.springframework.boot.autoconfigure.session.NonUniqueSessionRepositoryFailureAnalyzer
+     */
+    static void failureAnalyzers() {
 
     }
 
